44ecd28b55b08fce17c5b374376c9a60fe0a4d01
party <sholazar@mail.ru>
2016-07-23 13:01:51 +0300

    forms fix

--- theme/01.head.php
+++ theme/01.head.php
@@ -1,12 +1,7 @@
 <section id="">
     <div class="container-fluid">
         <div class="row">
-            <form action="#" id="form_a">
-                <input type="text">
-                <input type="text" name="phone">
-                <input type="file" name="file"><span></span>
-                <button type="submit">test</button>
-            </form>
+            
         </div>
     </div>
 </section>
