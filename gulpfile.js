0d15182b28f30b28d494931bb89b8febd4edf178
party <sholazar@mail.ru>
2016-08-01 19:33:22 +0300

    gulp add

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var uncss = require('gulp-uncss');
 var shorthand = require('gulp-shorthand');
 var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
+var autopref = require('autoprefixer');
 var minify = require('gulp-minify-css');
 
 gulp.task('sass', function () {
     gulp.src('sass/**/*.scss')
         .pipe(sass())
-        .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+        .pipe(postcss([autopref({browsers: ['last 2 versions']})]))
         .pipe(shorthand())
         .pipe(minify())
         .pipe(gulp.dest('css')) 
