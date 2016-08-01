0c82ccb8f83d46603ec6b3e5983a9da0892d2e36
party <sholazar@mail.ru>
2016-08-01 19:00:01 +0300

    gulp add

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uncss = require('gulp-uncss');
+var shorthand = require('gulp-shorthand');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var minify = require('gulp-minify-css');
+
+gulp.task('sass', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+        .pipe(shorthand())
+        .pipe(minify())
+        .pipe(gulp.dest('css'))
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+});
+
+function onError(err) {
+    console.log(err);
+}
+
+gulp.task('swatch', function () {
+    gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['swatch']);
